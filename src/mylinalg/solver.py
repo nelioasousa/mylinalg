8fd8007a690e61291aa7fe7644ff3ff7c18907e3
nelioasousa <corneliosousaphb@gmail.com>
2026-01-25 12:55:58 -0300

    Add substitution stubs

--- src/mylinalg/solver.py
+++ src/mylinalg/solver.py
@@ -2,18 +2,26 @@
 
 import numpy as np
 from mylinalg.decompositions import rref, lu
-from mylinalg.utils import Matrix, check_matrix
+from mylinalg.utils import NPMatrix, Matrix, check_matrix
+
+
+def backward_substitution(A: Matrix, b: Matrix) -> NPMatrix:
+    return
+
+
+def forward_substitution(A: Matrix, b: Matrix) -> NPMatrix:
+    return
 
 
 def gauss_jordan_solver(
     A: Matrix,
     b: Matrix,
-) -> Matrix:
+) -> NPMatrix:
     return
 
 
 def lu_solver(
     A: Matrix,
     b: Matrix,
-) -> Matrix:
+) -> NPMatrix:
     return
