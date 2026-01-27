923f3bdb2242de08172bdacd9e98c8bbf5fe1ad0
nelioasousa <corneliosousaphb@gmail.com>
2026-01-27 10:02:35 -0300

    Gauss-Jordan inverse method

--- src/mylinalg/solver.py
+++ src/mylinalg/solver.py
@@ -1,8 +1,21 @@
 """Linear equations solvers."""
 
 import numpy as np
-from mylinalg.decompositions import rref, lu
+from mylinalg.processing import rref
+from mylinalg.decompositions import lu
 from mylinalg.utils import NPMatrix, Matrix, check_matrix
+
+
+def matrix_inverse(A: Matrix) -> NPMatrix:
+    A = check_matrix(A)
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Singular matrix")
+    A_aug = np.concatenate((A, np.identity(m, dtype=A.dtype)), axis=1)
+    rr, pivots = rref(A_aug, pivoting="partial", return_pivots_loc=True)
+    if len(pivots) != m or pivots[-1] != (m, m):
+        raise ValueError("Singular matrix")
+    return rr[:, m:]
 
 
 def backward_substitution(A: Matrix, b: Matrix) -> NPMatrix:
