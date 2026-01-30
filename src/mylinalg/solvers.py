69e46bd068516e309d811a9581ac68563a7d68bb
nelioasousa <corneliosousaphb@gmail.com>
2026-01-29 21:20:35 -0300

    Back and forward substitution impl

--- src/mylinalg/solvers.py
+++ src/mylinalg/solvers.py
@@ -3,7 +3,7 @@
 import numpy as np
 from mylinalg.processing import rref
 from mylinalg.decompositions import lu
-from mylinalg.utils import NPMatrix, Matrix, check_matrix
+from mylinalg.utils import NPMatrix, Matrix, check_matrix, ZERO_TOL
 
 
 def matrix_inverse(A: Matrix) -> NPMatrix:
@@ -27,11 +27,35 @@ def matrix_inverse(A: Matrix) -> NPMatrix:
 
 
 def backward_substitution(A: Matrix, b: Matrix) -> NPMatrix:
-    return
+    A = check_matrix(A)
+    b = check_matrix(b)
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Non-square matrix")
+    if b.shape[0] != m:
+        raise ValueError("Shape mismatch between `A` and `b`")
+    solutions = np.zeros((n, b.shape[1]), dtype=A.dtype)
+    for i in range(m - 1, -1, -1):
+        if np.abs(A[i, i]) < ZERO_TOL:
+            continue
+        solutions[i, :] = (b[i, :] - A[[i], i + 1 :].dot(solutions[i + 1 :, :])) / A[i, i]
+    return solutions
 
 
 def forward_substitution(A: Matrix, b: Matrix) -> NPMatrix:
-    return
+    A = check_matrix(A)
+    b = check_matrix(b)
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Non-square matrix")
+    if b.shape[0] != m:
+        raise ValueError("Shape mismatch between `A` and `b`")
+    solutions = np.zeros((n, b.shape[1]), dtype=A.dtype)
+    for i in range(m):
+        if np.abs(A[i, i]) < ZERO_TOL:
+            continue
+        solutions[i, :] = (b[i, :] - A[[i], :i].dot(solutions[:i, :])) / A[i, i]
+    return solutions
 
 
 def gauss_jordan_solver(A: Matrix, b: Matrix) -> NPMatrix:
